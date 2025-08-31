1901b3e0d4a33cae70d492b4e43423d8414eef98
Kenta Tanaka <tanaka@mana.bi>
2025-09-01 00:44:49 +0900

    20250901 add jupyterlab-spreadsheet-editor

--- scripts/install_jupyter.sh
+++ scripts/install_jupyter.sh
@@ -30,7 +30,7 @@ fi
 # shellcheck source=/dev/null
 source /rocker_scripts/install_python.sh
 
-python3 -m pip install --no-cache-dir jupyter-rsession-proxy notebook jupyterlab jupyterhub jupyterlab-visualpython
+python3 -m pip install --no-cache-dir jupyter-rsession-proxy notebook jupyterlab jupyterhub jupyterlab-visualpython jupyterlab-spreadsheet-editor
 
 install2.r --error --skipmissing --skipinstalled -n "$NCPUS" remotes
 
